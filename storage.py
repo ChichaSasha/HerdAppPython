e2bd3522326b8a1dc278b4226bc9f7ca72c12bf7
ChichaSasha <sashachicha13@gmail.com>
2019-07-15 14:01:07 +0100

    Sasha

--- storage.py
+++ storage.py
@@ -14,86 +14,11 @@ def get_next_id():
 class Storage:
     def __init__(self):
         self.acts = []
-        self.every_day_acts = []
-        self.single_acts = []
-        self.not_every_day_acts = []
 
     def add_action(self, some_action):
         some_action.id = get_next_id() # new action, generate new id
 
         self.acts.append(some_action)
-        if some_action.days_counter > 1:
-            self.not_every_day_acts.append(some_action)
-        elif some_action.days_counter == 1:
-            self.every_day_acts.append(some_action)
-        else:
-            self.single_acts.append(some_action)
-
-    def get_list_from_to(self, start_time, end_time):
-        res = []
-
-        for i in self.single_acts:
-            if (i.start_time > start_time) and (i.start_time < end_time):
-                res.append(i)
-
-        for obj in self.every_day_acts:
-
-            if obj.start_time < start_time:
-                k = 0
-                while k < len(self.every_day_acts):
-                    i = start_time
-                    while i < end_time:
-                        a = copy.copy(self.every_day_acts[k]) # true way
-                        a.start_time = i
-                        res.append(a)
-                        i += 1
-                    k += 1
-            elif (obj.start_time > start_time) and (obj.start_time < end_time):
-                k = 0
-                while k < len(self.every_day_acts):
-                    i = start_time
-                    while i < end_time:
-                        a = Action(self.every_day_acts[k].id,
-                                   self.every_day_acts[k].name,
-                                   i,
-                                   self.every_day_acts[k].description,
-                                   self.every_day_acts[k].days_counter)
-                        res.append(a)
-                        i += 1
-                    k += 1
-            k = 0
-            for obj in self.not_every_day_acts:
-                if obj.start_time < start_time:
-                    while k < len(self.not_every_day_acts):
-                        i = start_time
-                        while i < end_time:
-                            diff = i - self.not_every_day_acts[k].start_time
-
-                            if diff % self.not_every_day_acts[k].days_counter == 0:
-                                a = Action(self.not_every_day_acts[k].id,
-                                           self.not_every_day_acts[k].name,
-                                           self.not_every_day_acts[k].description,
-                                           i,
-                                           self.not_every_day_acts[k].days_counter)
-                                res.append(a)
-                            i += 1
-                        k += 1
-                elif (obj.start_time > start_time) and (obj.start_time < end_time):
-                    while k < len(self.not_every_day_acts):
-                        i = start_time
-                        while i < end_time:
-                            diff = i - self.not_every_day_acts[k].start_time
-                            if diff % self.not_every_day_acts[k].days_counter == 0:
-                                a = Action(self.not_every_day_acts[k].id,
-                                           self.not_every_day_acts[k].name,
-                                           self.not_every_day_acts[k].description,
-                                           i,
-                                           self.not_every_day_acts[k].days_counter)
-                                res.append(a)
-                            i += 1
-                        k += 1
-
-        return sorted(res, key=lambda action: action.start_time)
 
     def get_action_by_id(self, some_id):
         for p in self.acts:
@@ -106,12 +31,83 @@ class Storage:
         for i in self.acts:
             if i.id == some_id:
                 self.acts = [action for action in self.acts if action.id != some_id]
-                if i.days_counter == 1:
-                    self.every_day_acts = [action for action in self.every_day_acts if action.id != some_id]
-                elif i.days_counter > 1:
-                    self.not_every_day_acts = [action for action in self.not_every_day_acts if action.id != some_id]
-                else:
-                    self.single_acts = [action for action in self.single_acts if action.id != some_id]
                 return
 
         raise EventNotFound(some_id)
+
+    def get_not_every_day_acts(self):
+        return [event for event in self.acts if event.days_counter > 1]
+
+    def get_every_day_acts(self):
+        return [event for event in self.acts if event.days_counter == 1]
+
+    def get_single_acts(self):
+        return [event for event in self.acts if event.days_counter == 0]
+
+
+class Handler():
+
+    def init(self,storage):
+        self.storage = storage
+        self.not_every_day_acts = self.storage.get_not_every_day_acts()
+        self.every_day_acts = self.storage.get_every_day_acts()
+        self.single_day_acts = self.storage.get_single_acts()
+
+    def get_list_from_to(self,start_time,end_time):
+            res = []
+            for i in self.single_acts:
+                if (i.start_time > start_time) and (i.start_time < end_time):
+                    res.append(i)
+
+            for obj in self.every_day_acts:
+
+                if obj.start_time < start_time:
+                    k = 0
+                    while k < len(self.every_day_acts):
+                        i = start_time
+                        while i < end_time:
+                            a = copy.copy(self.every_day_acts[k])  # true way
+                            a.start_time = i
+                            res.append(a)
+                            i += 1
+                        k += 1
+                elif (obj.start_time > start_time) and (obj.start_time < end_time):
+                    k = 0
+                    while k < len(self.every_day_acts):
+                        i = start_time
+                        while i < end_time:
+                            a = copy.copy(self.every_day_acts[k])  # true way
+                            a.start_time = i
+                            res.append(a)
+                            i += 1
+                        k += 1
+                k = 0
+                for obj in self.not_every_day_acts:
+                    if obj.start_time < start_time:
+                        while k < len(self.not_every_day_acts):
+                            i = start_time
+                            while i < end_time:
+                                diff = i - self.not_every_day_acts[k].start_time
+
+                                if diff % self.not_every_day_acts[k].days_counter == 0:
+                                    a = copy.copy(self.every_day_acts[k])  # true way
+                                    a.start_time = i
+                                    res.append(a)
+                                i += 1
+                            k += 1
+                    elif (obj.start_time > start_time) and (obj.start_time < end_time):
+                        while k < len(self.not_every_day_acts):
+                            i = start_time
+                            while i < end_time:
+                                diff = i - self.not_every_day_acts[k].start_time
+                                if diff % self.not_every_day_acts[k].days_counter == 0:
+                                    a = copy.copy(self.every_day_acts[k])  # true way
+                                    a.start_time = i
+                                    res.append(a)
+                                i += 1
+                            k += 1
+
+            return sorted(res, key=lambda action: action.start_time)
+
+
+
